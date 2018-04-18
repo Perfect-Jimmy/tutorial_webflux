a622d80965e31c11bf6c7869869e259d237b562e
jimmy <773792867@qq.com>
2018-04-18 14:44:27 +0800

    Initial commit

---
+++ tutorial_fluxapp/src/main/java/com/webflux/WebFluxStart.java
@@ -0,0 +1,16 @@
+package com.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Jimmy. 2018/4/18  12:51
+ */
+@SpringBootApplication
+//@EnableWebFluxSecurity
+public class WebFluxStart {
+
+    public static void main(String[] args) {
+        //SpringApplication.run(WebFluxStart.class);
+    }
+}
